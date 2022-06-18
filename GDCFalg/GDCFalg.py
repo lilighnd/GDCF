dccac36259171776fd3dfc30edc7ebd0ea203b6e
lilighnd <ghanadzadehleila5@gmail.com>
2022-06-18 20:46:26 +0430

    Add : print labels and RandIndex

--- GDCFalg/GDCFalg.py
+++ GDCFalg/GDCFalg.py
@@ -270,6 +270,9 @@ alltime=time.time() - start_time
 R1=adjusted_rand_score(True_label, Pred_label)
 R2=adjusted_rand_score(True_label, db.labels_)
 
+print(True_label, Pred_label,db.labels_)
+print(R1,R2)
+
 M1=adjusted_mutual_info_score(True_label, Pred_label)
 M2=adjusted_mutual_info_score(True_label, db.labels_)
 
