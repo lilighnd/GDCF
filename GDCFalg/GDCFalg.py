6d2884eeb7fbcce07306d3feaa6430896ce49a49
lilighnd <ghanadzadehleila5@gmail.com>
2022-06-23 20:22:36 +0430

    add : print

--- GDCFalg/GDCFalg.py
+++ GDCFalg/GDCFalg.py
@@ -46,8 +46,8 @@ n_samples =38000
 #                   cluster_std = 0.4,
 #                   shuffle = True)
 
-blobs = datasets.make_blobs(n_samples=n_samples, n_features = 2, 
-                  centers = 3,cluster_std = 0.4)
+# blobs = datasets.make_blobs(n_samples=n_samples, n_features = 2, 
+#                   centers = 3,cluster_std = 0.4)
 
 #no_structure = np.random.rand(n_samples, 2), None
 
@@ -65,10 +65,10 @@ blobs = datasets.make_blobs(n_samples=n_samples, n_features = 2,
 
 
 # --------------------------------------------------------------------------------------------------
-# m = DataSet.Test30()
-# True_label = m[1]
-# m = m[0].Data
-
+m = DataSet.Test30()
+True_label = m[1]
+m = m[0].Data
+print(m[0][0])
 # print("load m")
 
 # ------Read Data another way---------
@@ -81,8 +81,8 @@ blobs = datasets.make_blobs(n_samples=n_samples, n_features = 2,
 #True_label = df.values.tolist()
 
 
-True_label = blobs[1]
-m=blobs[0]
+# True_label = blobs[1]
+# m=blobs[0]
 Data= [[] for i in range(len(m[0]))]
 for dim in range(len(m[0])):
     for i in range(len(m)):
@@ -109,15 +109,16 @@ print("load Data")
 #    for data in Data[0]:
 #        D[dim].append(data[dim])
 
-np.savetxt("/content/drive/MyDrive/Colab Notebooks/blobsData1m.csv", 
-          m,
-          delimiter =",", 
-          fmt ='% s')
+# np.savetxt("/content/drive/MyDrive/Colab Notebooks/blobsData1m.csv", 
+#           m,
+#           delimiter =",", 
+#           fmt ='% s')
 
-np.savetxt("/content/drive/MyDrive/Colab Notebooks/blobsLabel1m.csv", 
-          True_label,
-          delimiter =",", 
-          fmt ='% s')
+# np.savetxt("/content/drive/MyDrive/Colab Notebooks/blobsLabel1m.csv", 
+#           True_label,
+#           delimiter =",", 
+#           fmt ='% s')
+
 # np.savetxt("blobsData150.csv",
 #   Data[0],
 #   delimiter =",",
