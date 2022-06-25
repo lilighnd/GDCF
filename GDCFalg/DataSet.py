8a4761304e0df1d55a7543c2a86c071f44326460
lilighnd <ghanadzadehleila5@gmail.com>
2022-06-25 16:52:25 +0430

    Update : seeds  and fix Y_test

--- GDCFalg/DataSet.py
+++ GDCFalg/DataSet.py
@@ -113,8 +113,11 @@ class DataSet:
 
 
 
-        X_train,X_test,Y_train,Y_test = train_test_split(data,True_label,test_size=0.33)
-        test_list = [int(i) for i in Y_test]
+        X_train,X_test,Y_train,Y_test = train_test_split(data,True_label,test_size=0.33,random_state=42)
+        seed_val=42
+        train_test_split.seed(seed_val)
+        test_list = [int(i) for i in Y_test[:][0]]
+        print(test_list)
         print(type(X_test[0][0]),type(Y_test[0]))
         print(type(data[0]),type(True_label[0]))
 
