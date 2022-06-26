7c082d19c561ac30602bf41df8772beab5869a0c
lilighnd <ghanadzadehleila5@gmail.com>
2022-06-26 15:44:08 +0430

    u

--- GDCFalg/Make_Square.py
+++ GDCFalg/Make_Square.py
@@ -57,7 +57,7 @@ class Make_Square():
 
                 inner_d = poly.contains_points(self.Data)
                 inner_data = np.where(inner_d)
-                print(f"inner data : {inner_d},{type(inner_d)}")
+                print(f"inner d : {inner_d},{type(inner_d)}")
                 print(f"inner data : {inner_data},{type(inner_data)}")
                 DataInGrid.append(list(inner_data))
 
