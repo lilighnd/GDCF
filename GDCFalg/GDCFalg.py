03e1763886109a7e92aa0590e57bc22a907aee29
lilighnd <ghanadzadehleila5@gmail.com>
2022-06-28 17:56:36 +0430

    u1

--- GDCFalg/GDCFalg.py
+++ GDCFalg/GDCFalg.py
@@ -317,12 +317,11 @@ if modeGrid == int(1):
     s_obj["datagrid"] = gridData
 
     save_object = json.dumps(s_obj["grid"],indent=13)
+    save_object = json.dumps(s_obj["datagrid"],indent=13)
     with open("/content/drive/MyDrive/Colab Notebooks/saveobject.json", "w") as outfile:
         outfile.write(save_object)
 
-    save_object = json.dumps(s_obj["datagrid"])
-    with open("/content/drive/MyDrive/Colab Notebooks/saveobject.json", "w") as outfile:
-        outfile.write(save_object)
+    
 
     print("run grid")
 
