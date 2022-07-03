31c110b77a3c2ff388ca17f0752dcadc10a9a058
lilighnd <ghanadzadehleila5@gmail.com>
2022-07-03 13:15:02 +0430

    u1

--- GDCFalg/NeighbourHex.py
+++ GDCFalg/NeighbourHex.py
@@ -34,7 +34,7 @@ class NeighbourHex():
             g = x * self.numGx + y
             if g <= len(tmp1):
                 tmp1[g] = 0
-            print(f"NHex x,y : {x,y,g,tmp1 }")
+            print(f"NHex x,y : {x,y,self.numGx,g,tmp1 }")
 
             x = self.g[0] - 2
             y = self.g[1] + 2
