69dddf0d7334f6a965d094e22edc90d92f907868
lilighnd <ghanadzadehleila5@gmail.com>
2022-07-04 13:05:15 +0430

    Update : save data

--- GDCFalg/GDCFalg.py
+++ GDCFalg/GDCFalg.py
@@ -202,7 +202,7 @@ for dim in range(len(m[0])):
     for i in range(len(m)):
         Data[dim].append(m[i][dim])
 
-np.savetxt("data.csv",Data,delimiter=' ')
+np.savetxt("..\\DataSet\\data.csv",Data,delimiter=' ')
 # print(f"Data : {Data,len(Data),len(Data[0])}")
 # print("load Data")
 
