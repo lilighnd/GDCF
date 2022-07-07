bdf8aeea55822dc342499a8cd4ebd32bec2941c4
lilighnd <ghanadzadehleila5@gmail.com>
2022-07-07 10:06:59 +0430

    u

--- GDCFalg/make_Hex.py
+++ GDCFalg/make_Hex.py
@@ -31,7 +31,7 @@ class make_Hex():
         numGridX = int(np.ceil(((maxX-minX)/(3*(self.Eps/2)))*2))  # ==> 5
         numGridY = int(np.ceil((maxY-minY)/(0.866*self.Eps)))  # ==> 4
         GX=int(numGridX * numGridY)
-        # print(f"xgrid,ygrid,allgrid : {numGridX,numGridY,GX}")
+        print(f"xgrid,ygrid,allgrid : {numGridX,numGridY,GX}")
         # print(f"all grid : {GX}")
         d1=[]
         d2=[]
