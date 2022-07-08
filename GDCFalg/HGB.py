8c1111d3dd8732e6f122b40001da16b36b8b1c2b
lilighnd <ghanadzadehleila5@gmail.com>
2022-07-08 18:46:26 +0430

    u

--- GDCFalg/HGB.py
+++ GDCFalg/HGB.py
@@ -14,15 +14,19 @@ class HGB():
                     Max[0]=self.nonemptyGrids[j][0]
                 if self.nonemptyGrids[j][1]>Max[1]:
                     Max[1]=self.nonemptyGrids[j][1]
-            #b=np.zeros(((self.S[-1][i]+1),len(self.S)))
             b=np.zeros((Max[i]+1,len(self.nonemptyGrids)))
             B.append(b)
 
+        # numG[numGX,numGY]
+        # for i in range(self.d):
+        #     b=np.zeros((numG[i]+1,len(self.nonemptyGrids)))
+        #     B.append(b)
+
+
         for g in self.nonemptyGrids:
-            if len(g)==3: 
-                for i in range(self.d):
-                    pos=g[i]
-                    B[i][pos][k]=1
+            for i in range(self.d):
+                pos=g[i]
+                B[i][pos][k]=1
             k+=1
         # print(f"hgb : {B[0][4]}")
         return B
