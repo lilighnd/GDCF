46867ea2d9425717dd38f14aab81e533334c4602
lilighnd <ghanadzadehleila5@gmail.com>
2022-07-08 12:37:17 +0430

    Update : Change Grid to nonemptyGrid

--- GDCFalg/HGB.py
+++ GDCFalg/HGB.py
@@ -2,24 +2,23 @@ import numpy as np
 class HGB():
     """HGB PROCCESS"""
     def __init__(self,s,dim):
-        self.Grids=s
-        print(f"self.Grids : {self.Grids}")
+        self.nonemptyGrids=s
         self.d=dim
     def BuildHGB(self):
         k=0
         B=[]
         Max=[0,0]
         for i in range(self.d):
-            for j in range(len(self.Grids)):
-                if self.Grids[j][0]>Max[0]:
-                    Max[0]=self.Grids[j][0]
-                if self.Grids[j][1]>Max[1]:
-                    Max[1]=self.Grids[j][1]
+            for j in range(len(self.nonemptyGrids)):
+                if self.nonemptyGrids[j][0]>Max[0]:
+                    Max[0]=self.nonemptyGrids[j][0]
+                if self.nonemptyGrids[j][1]>Max[1]:
+                    Max[1]=self.nonemptyGrids[j][1]
             #b=np.zeros(((self.S[-1][i]+1),len(self.S)))
-            b=np.zeros((Max[i]+1,len(self.Grids)))
+            b=np.zeros((Max[i]+1,len(self.nonemptyGrids)))
             B.append(b)
 
-        for g in self.Grids:
+        for g in self.nonemptyGrids:
             if len(g)==3: 
                 for i in range(self.d):
                     pos=g[i]
