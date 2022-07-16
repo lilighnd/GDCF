51791fbfd50a45c6858f35954ca324941123fe13
lilighnd <ghanadzadehleila5@gmail.com>
2022-07-16 15:46:23 +0430

    u

--- GDCFalg/GDCFalg.py
+++ GDCFalg/GDCFalg.py
@@ -383,7 +383,7 @@ print(f"R1,alltime : {R1,alltime}")
 
 
 #----------------------------------------xlsxwriter------------------------------------------
-namefile = str(json_object["data"]) + str(json_object["n_samples"]) + '.xls'
+namefile = str(json_object["data"]) + str(json_object["n_samples"])
 
 ls = []
 ls.append(json_object["Eps"])
@@ -391,7 +391,7 @@ ls.append(R1)
 ls.append(alltime)
 df = pd.DataFrame(ls) 
 
-excel_name = f'/content/drive/MyDrive/Colab Notebooks/namefile'
+excel_name = f'/content/drive/MyDrive/Colab Notebooks/"namefile".xls'
 print(excel_name)
 df_source = None
 if os.path.exists(excel_name):
