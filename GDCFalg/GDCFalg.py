3996d317b6d7e5d1bc330f2dcea5e11dbd0870e1
lilighnd <ghanadzadehleila5@gmail.com>
2022-07-19 20:56:38 +0430

    u

--- GDCFalg/GDCFalg.py
+++ GDCFalg/GDCFalg.py
@@ -211,7 +211,7 @@ m = DataSet.data()
 True_label = m[1]
 m = m[0].Data#type of m and True_labels is List
 Data=np.transpose(np.array(m))#type of Data is array and Data is transpose of m
-
+print(m[0])
 # ------Read Data another way---------
 
 
