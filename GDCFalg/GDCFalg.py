0f00e562f48febaa89a9e77968b5c70896e982a3
lilighnd <ghanadzadehleila5@gmail.com>
2022-07-19 12:29:02 +0430

    u

--- GDCFalg/GDCFalg.py
+++ GDCFalg/GDCFalg.py
@@ -197,8 +197,8 @@ m = pd.DataFrame(pd.read_excel(f'/content/drive/MyDrive/Colab Notebooks/datawith
 
 
 m = DataSet.data()
-print(m[0])
-print(type(m[0]))
+print(m[0].Data)
+print(type(m[0].Data))
 True_label = m[1]
 m = m[0].Data#type of m and True_labels is List
 Data=np.transpose(np.array(m))#type of Data is array and Data is transpose of m
