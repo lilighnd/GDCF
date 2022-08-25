a254b5224d2417dd5626d87f5f409762b21eef93
lilighnd <ghanadzadehleila5@gmail.com>
2022-08-25 15:13:16 +0430

    u

--- GDCFalg/CoreGrids.py
+++ GDCFalg/CoreGrids.py
@@ -21,7 +21,7 @@ class CoreGrids:
         # dist = distance.cdist([Point], self.m, 'euclidean')
         # dist = distance.cdist([Point], self.m, 'chebyshev')
         # dist = distance.cdist([Point], self.m, 'cityblock')
-        dist = distance.cdist([Point], self.m, 'seuclidean')
+        dist = distance.cdist([Point], self.m, 'sqeuclidean')
         return dist
 
     def Find_CoreObject(self):
