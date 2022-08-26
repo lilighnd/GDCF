89c3e0e9330c92e69fd5f71314da4451e2e403be
lilighnd <ghanadzadehleila5@gmail.com>
2022-08-26 21:14:48 +0430

    p

--- GDCFalg/GDCFalg.py
+++ GDCFalg/GDCFalg.py
@@ -237,10 +237,11 @@ with open('/content/drive/MyDrive/Colab Notebooks/inputobject.json', 'r') as ope
 
 dim = int(json_object["features"])
 m = DataSet.data()
-print(m)
 True_label = m[1]
 m = m[0].Data#type of m and True_labels is List
 Data=np.transpose(np.array(m))#type of Data is array and Data is transpose of m
+print(m)
+
 #-----------------------------------start iris---------------------------------------------
 # m=[[5.1,3.5,1.4,0.2],
 # [4.9,3,1.4,0.2],
