b01c7de7c5bae7dd11eac49519df1758312593b2
lilighnd <ghanadzadehleila5@gmail.com>
2022-08-27 16:59:29 +0430

    d

--- GDCFalg/DataSet.py
+++ GDCFalg/DataSet.py
@@ -216,8 +216,11 @@ class DataSet:
             excel_name_label = f'/content/drive/MyDrive/Colab Notebooks/mydata.xlsx'
             df_data = pd.DataFrame(data[0])
             df_data.to_excel(excel_name_label,index=False)
-        # True_label = data[1]
-        # Data=data[0]
+        
+        True_label = data[1]
+        Data=data[0]
+        print(True_label)
+        print(Data)
         #for 1-dim
         # Data=normalized#for 3-dim
         # Data=datand#for n-dimention data
