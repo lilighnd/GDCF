a8e3b2615bc1bae49595b3310d54a1661f988ab7
lilighnd <ghanadzadehleila5@gmail.com>
2022-09-10 19:17:31 +0430

    ع

--- GDCFalg/GDCF.py
+++ GDCFalg/GDCF.py
@@ -278,14 +278,14 @@ def mergability(self, ind_g, ind_gprim, DataGrids, Data):
     # m=[[],[]]
     for data in range(len(DataGrids[ind_g])):
         for dataNeighbourGrid in range(len(DataGrids[ind_gprim])):
-            # dist = distance.cityblock(
-            #     Data[DataGrids[ind_g][data]], Data[DataGrids[ind_gprim][dataNeighbourGrid]])  # Calculate euclidean
+            dist = distance.cityblock(
+                Data[DataGrids[ind_g][data]], Data[DataGrids[ind_gprim][dataNeighbourGrid]])  # Calculate euclidean
             # dist = distance.minkowski(
             #     Data[DataGrids[ind_g][data]], Data[DataGrids[ind_gprim][dataNeighbourGrid]])  # Calculate euclidean
             # dist = distance.chebyshev(
             #     Data[DataGrids[ind_g][data]], Data[DataGrids[ind_gprim][dataNeighbourGrid]])  # Calculate euclidean
-            dist = distance.euclidean(
-                Data[DataGrids[ind_g][data]], Data[DataGrids[ind_gprim][dataNeighbourGrid]])  # Calculate euclidean
+            # dist = distance.euclidean(
+            #     Data[DataGrids[ind_g][data]], Data[DataGrids[ind_gprim][dataNeighbourGrid]])  # Calculate euclidean
             # dist = distance.jaccard(
             #     Data[DataGrids[ind_g][data]], Data[DataGrids[ind_gprim][dataNeighbourGrid]])  # Calculate euclidean
             # print(f"dist: {dist}")
